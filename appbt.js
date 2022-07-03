ffc3403cfef3632471db4023ee4a69c4ec333d35
Amirabbas <amir.momeni.gelloria79@gmail.com>
2022-07-04 01:08:20 +0430

    Files Comited v1

---
+++ appbt.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+
+
+bot_secret_token = "YOUR DISCORD TOKEN"
+
+const d = new Date();
+
+client.on('ready', () => {
+
+  // Run Log in your terminal with date & time :D
+  console.log(`Logged in as ${client.user.tag}! on ${d}`);
+
+  // Activity of Your Bot e.g. => Developing Demon Bot#2121
+  client.user.setActivity(`Developing The ${client.user.tag} !`)
+});
+
+client.on('message', message => {
+
+
+    if (message.content === 'ping') {  
+      message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+    }else if(message.content == 'hi'){
+        message.channel.send(`Salam Be Ruye Mahet`);
+    }else if(message.content == 'developer'){
+        message.channel.send(`Amirabbas Momeni :D`);
+    }
+  });
+
+
+client.login(bot_secret_token)
